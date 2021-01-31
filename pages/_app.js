a5f3716218177ef1127b85650aeb67bd172a80cf
Eula <eula.silva.teixeira@gmail.com>
2021-01-31 13:33:49 -0300

    Ajustes

--- pages/_app.js
+++ pages/_app.js
@@ -38,6 +38,13 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+        <title>App Quiz</title>
+
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/quiz.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/quiz.png" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
